b3182b3578e91751877fa9378be33e15da6791a7
victor_cordova <victorcordovapoma@gmail.com>
2022-06-15 09:55:56 -0500

    Added dist

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import Template from 'Templates/template.js';
-
-import "Styles/main.css";
-
-(async function App() {
-  const main = null || document.getElementById('main');
-  main.innerHTML = await Template();
-})();
